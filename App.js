700740c579bbf9cab95a6fd21228f29f30b5fc9d
Nobita Nguyen <nguyenhoa072@gmail.com>
2022-08-21 20:52:50 +0700

    update google

--- App.js
+++ App.js
@@ -6,114 +6,67 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import type { Node } from 'react';
 import SplashScreen from 'react-native-splash-screen';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Button } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import auth from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    GoogleSignin.configure({
+        webClientId:
+            '256408849779-j4lkku6suka4pt9aq3782vpe86he5d9r.apps.googleusercontent.com',
+    });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const onGoogleButtonPress = async () => {
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
 
-  useEffect(() => {
-    // To hide splash screen
-    SplashScreen.hide();
-  }, []);
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        // Sign-in the user with the credential
+        const user_sign_in = auth().signInWithCredential(googleCredential);
+
+        user_sign_in
+            .then(user => {
+                console.log('user', user);
+            })
+            .catch(error => {
+                console.log('error', error);
+            });
+    };
+
+    const logOut = async () => {
+        auth()
+            .signOut()
+            .then(() => console.log('User signed out!'))
+    }
+
+    useEffect(() => {
+        // To hide splash screen
+        SplashScreen.hide();
+    }, []);
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Button
+                onPress={onGoogleButtonPress}
+                title="Sign in with Google"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+                onPress={logOut}
+                title="Signing out"
+                color="#ff0000"
+                accessibilityLabel="Learn more about this purple button"
+            />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
